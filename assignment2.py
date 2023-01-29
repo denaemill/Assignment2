7aec3e1bfd888058e894ebf403ea37926845c3d8
denaemill <114959977+denaemill@users.noreply.github.com>
2023-01-29 16:34:41 -0500

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -70,24 +70,30 @@ class Assignment2:
     # Task 6
     @staticmethod
     def connectTcp(host, port):
-        HOST = socket.gethostbyname(host)
+        host = socket.gethostbyname(host)
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
-        sock.bind((HOST, port))
-        sock.listen(5)
+        sock.bind((host, port))
+        sock.listen(1)
         print('Waiting for incoming connections on', sock)
         conn, addr = sock.accept()
 
         print('Connected by', addr)
         while True:
             data = conn.recv(1024)
+            print(data)
 
             if not data:
                 break
-
             conn.send(data)
 
         conn.close()
         sock.close()
         return True
+
+
+##if retval:
+    #print("Connection established correctly")
+#else:
+    #print("Some error")
